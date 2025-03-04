c21c8dfc89be589f5ab76185c507df4ad2124ee6
simon-shao-ics4u1-03-25 <simon.shao@student.tdsb.on.ca>
2025-03-04 19:28:44 +0000

    Edit divide() to specify arithmetic exception

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,6 @@
 public class PracticeProblem {
 
 	public static void main(String args[]) {
-		int[] numArr1 = new int[] {1, 2, 3, 4, 5};
-System.out.print(safeConvertStringtoInt("hello"));
 	}
 public static boolean validIndex(int[] numArr, int num){
 	try{
@@ -19,8 +17,8 @@ public static int divide(int num, int num1){
 try{
 	return num / num1;
 }
-catch(Exception e){
-return 0;
+catch(ArithmeticException e){
+	return 0;
 }
 }
 
